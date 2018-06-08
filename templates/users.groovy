b2d005448f78f059f5aa3284348d3fbf1c6f3920
Dmitry Burmistrov <dburmistrov@mirantis.com>
2018-06-08 16:28:30 +0400

    Refactor users section

---
+++ templates/users.groovy
@@ -0,0 +1,107 @@
+#!groovy
+
+import jenkins.model.Jenkins
+import hudson.model.User
+import hudson.security.HudsonPrivateSecurityRealm.Details
+
+class Actions {
+    Actions(out) {
+        this.out = out
+        this.instance = Jenkins.instance
+        this.userList = User.getAll()
+    }
+
+    def out
+    def instance
+    def userList
+    Boolean changed = false
+    def defaultUser = [present: true]
+
+    def classLoader = Jenkins.getInstance().pluginManager.uberClassLoader
+
+    void configure(params) {
+        def userParams
+        params.each { name, value ->
+            userParams = defaultUser + (value ?: [:])
+
+            if (userParams.present) {
+                setUser(name, defaultUser + (value ?: [:]))
+            } else {
+                removeUser(name)
+            }
+        }
+    }
+
+    void setUser(name, params) {
+        def user = userList.find { it.id == name }
+        def details = user ? user.getProperty(Details) : null
+        if (!(user && details)) {
+            user = Jenkins.getInstance().securityRealm
+                .createAccount(name, params.password)
+            details = user.getProperty(Details)
+            changed = true
+        }
+        if (user) {
+            if (params.full_name && user.fullName != params.full_name) {
+                user.setFullName(params.full_name)
+                changed = true
+            }
+            if (params.description && user.description != params.description) {
+                user.setDescription(params.description)
+                changed = true
+            }
+            if (params.password && !details.isPasswordCorrect(params.password)) {
+                user.addProperty(Details.fromPlainPassword(params.password))
+                changed = true
+            }
+            if (params.ssh_key) {
+               Class<?> sshClass = classLoader.loadClass('org.jenkinsci.main.modules.cli.auth.ssh.UserPropertyImpl')
+               def sshProperty = user.getProperty(sshClass)
+               def authorizedKeys = sshProperty ? sshProperty.authorizedKeys : ''
+               if (authorizedKeys != params.ssh_key) {
+                   def sshConstructor = sshClass.getDeclaredConstructor(String)
+                   user.addProperty(sshConstructor.newInstance(params.ssh_key))
+                   changed = true
+               }
+            }
+            if (params.email) {
+                def newEmailProperty
+                // Test Mailer plugin installed
+                try {
+                    newEmailProperty = new hudson.tasks.Mailer.UserProperty(params.email)
+                } catch (Exception e) {
+                    out.println 'FAILED'
+                    newEmailProperty = null
+                }
+                if (newEmailProperty) {
+                    def emailProperty = user.getProperty(hudson.tasks.Mailer.UserProperty)
+                    if ((emailProperty.getAddress() ?: '') != params.email) {
+                        user.addProperty(newEmailProperty)
+                        changed = true
+                    }
+                }
+            }
+        }
+    }
+
+    void removeUser(name) {
+        def user = userList.find { it.id == name }
+        def details = user ? user.getProperty(Details) : null
+        if (details) {
+            user.delete()
+            changed = true
+        }
+    }
+}
+
+def users = new groovy.json.JsonSlurperClassic()
+    .parseText('''{{ jenkins.users | to_json}}''')
+def actions = new Actions(out)
+
+actions.configure(users)
+
+if (actions.changed) {
+    println 'CHANGED'
+} else {
+    println 'EXISTS'
+}
